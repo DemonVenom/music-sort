4086666b85b2d6a0ff7bc9c4f186610c9b5825a5
Henry Hoffman <ehoffmn9@outlook.com>
2024-11-02 20:20:11 -0400

    added manual sorting of songs

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let lines = null;
 let embedContainer = null;
 const delay = 250;
 let index = 0;
+let dragged;
+let id;
+let dragIndex;
+let indexDrop;
+let list;
 
 document.getElementById('csvFileInput').addEventListener('change', (event) => {
     const file = event.target.files[0];
@@ -31,7 +36,15 @@ function createIframe() {
             const iframe = document.createElement('iframe');
             iframe.src = embedUrl;
             iframe.title = `Spotify Embed: ${columns[1]} by ${columns[2]}`;
-            embedContainer.appendChild(iframe);
+            const listItem = document.createElement('li');
+            listItem.draggable = true;
+            listItem.className = "dropzone"
+            listItem.id = index;
+            const icon = document.createElement('i');
+            icon.classList.add('fas', 'fa-grip-vertical');
+            listItem.appendChild(icon);
+            listItem.appendChild(iframe);
+            embedContainer.appendChild(listItem);
         }
     }
 
@@ -48,3 +61,35 @@ function parseCSV(data) {
 
     createIframe();
 }
+
+
+document.addEventListener("dragstart", ({target}) => {
+    dragged = target;
+    id = target.id;
+    list = target.parentNode.children;
+    for(let i = 0; i < list.length; i += 1) {
+      if (list[i] === dragged) {
+        dragIndex = i;
+      }
+    }
+});
+
+document.addEventListener("dragover", (event) => {
+    event.preventDefault();
+});
+
+document.addEventListener("drop", ({target}) => {
+ if(target.className == "dropzone" && target.id !== id) {
+     dragged.remove( dragged );
+    for (let i = 0; i < list.length; i += 1) {
+      if (list[i] === target) {
+        indexDrop = i;
+      }
+    }
+    if (dragIndex > indexDrop) {
+      target.before( dragged );
+    } else {
+     target.after( dragged );
+    }
+  }
+});
